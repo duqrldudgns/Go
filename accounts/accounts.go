fc8a1487d9ef86f179e89e0af4836f881b4c6a32
진영훈 <duqrldudgns@gmail.com>
2021-05-21 00:27:06 +0900

    update

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,6 +1,9 @@
 package accounts
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Account struct
 type Account struct {
@@ -34,4 +37,19 @@ func (a *Account) Withdraw(amount int) error {
 
 	a.balance -= amount
 	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	//return "whatever you want"
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/duqrldudgns/learngo/accounts"
+	"github.com/duqrldudgns/Go/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		log.Fatalln(err)
-		//fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
