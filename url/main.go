a58ecbf46407e4717ee01a85c5261b6c166f3bd1
진영훈 <duqrldudgns@gmail.com>
2021-05-24 01:23:34 +0900

    Update url

--- url/main.go
+++ url/main.go
@@ -1,40 +1,47 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-
 	"net/http"
 )
 
-var errRequestFailed = errors.New("Request failed")
+type requestResult struct {
+	url    string
+	status string
+}
+
+//var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	var results = map[string]string{}
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.google.com/",
 		"https://www.naver.com/",
 		"https://www.youtube.com/",
+		"https://www.facebook.com/",
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
 	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
