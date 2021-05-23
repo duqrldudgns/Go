7516bed517680ab1ce8299068d12fff609101f35
진영훈 <duqrldudgns@gmail.com>
2021-05-23 22:48:46 +0900

    update

--- main.go
+++ main.go
@@ -6,13 +6,19 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	sexyCount("flynn")
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for", i)
+		fmt.Println(<-c)
+	}
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + "is sexy"
 }
